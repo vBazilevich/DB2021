8f9026a0ff6de676c32f337a25bba831c46a3277
vladimir <v.bazilevich@innopolis.university>
2021-02-21 16:28:39 +0300

    Fixed code style issues

--- lab5/ex1.py
+++ lab5/ex1.py
@@ -1,7 +1,6 @@
 from faker import Faker
 import psycopg2
 import sys
-import string
 import random
 
 # CONSTANTS secion
@@ -11,6 +10,8 @@ PASS = 'password'
 HOST = '127.0.0.1'
 PORT = '5432'
 CUSTOMERS = 100000
+CREATE_BTREE_INDEX = True
+CREATE_HASH_INDEX = True
 
 print('Establishing connection to database...')
 try:
@@ -19,28 +20,31 @@ except psycopg2.Error as e:
     print(e.pgcode)
     print(e.pgerror)
     exit(-1)
+else:
+    print('Connection established.')
 
-print('Connection established.')
 cursor = con.cursor()
 
 print('Creating table customers')
 try:
     cursor.execute('DROP TABLE IF EXISTS customers;')
-    cursor.execute(''' CREATE TABLE IF NOT EXISTS customers
+    cursor.execute(''' CREATE TABLE customers
                     (ID SERIAL PRIMARY KEY,
                      NAME TEXT NOT NULL,
                      ADDRESS TEXT NOT NULL,
                      AGE INT NOT NULL,
                      REVIEW TEXT NOT NULL);''')
-    cursor.execute('CREATE INDEX age_index ON customers (AGE);')
-    cursor.execute('CREATE INDEX name_index ON customers USING hash (NAME);')
+    if CREATE_BTREE_INDEX:
+        cursor.execute('CREATE INDEX age_index ON customers (AGE);')
+    if CREATE_HASH_INDEX:
+        cursor.execute('CREATE INDEX name_index ON customers USING hash (NAME);')
     con.commit()
 except psycopg2.Error as e:
     print(e.pgcode)
     print(e.pgerror)
     exit(-1)
-
-print('Table customers is created')
+else:
+    print('Table customers is created')
 
 fake = Faker()
 def insert_random_customer(cursor):
@@ -58,6 +62,7 @@ def insert_random_customer(cursor):
 print(f"Instantiating table with {CUSTOMERS} customers")
 for _ in range(CUSTOMERS):
     insert_random_customer(cursor)
+
 try:
     con.commit()
 except psycopg2.Error as e:
@@ -67,7 +72,7 @@ except psycopg2.Error as e:
 else:
     print('Database instantiated successfully.')
 
-print("Adding guest customer for testing hash index")
+print("Adding special customer for testing hash index")
 try:
     cursor.execute("INSERT INTO customers (NAME, ADDRESS, AGE, REVIEW) VALUES ('Vlad Minkin', 'Russia, Tatarstan', 42, 'Good job!');")
     con.commit()
